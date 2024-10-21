1bd0772c115d5ffeb8cb4f7e90414f5d163c5cd5
Work Goonja <workgoonja@goonjas-MacBook-Pro.local>
2024-10-21 13:46:39 -0400

    added a more efficient way to solve the problem

--- CheckingDuplicates.js
+++ CheckingDuplicates.js
@@ -3,16 +3,15 @@
  * @return {boolean}
  */
 var containsDuplicate = function(nums) {
-    if(nums.length < 2) return false;
+   let seen = new Set();
 
-    for(let i = 0; i < nums.length; i++) {
-        for(let j = i+1; j < nums.length; j++) {
-            if(nums[i] === nums[j]) {
-                return true;
-            }
+   for(let i = 0; i < nums.length; i++) {
+        if(seen.has(nums[i])) {
+            return true
         }
-    }
-    return false;
+        seen.add(nums[i])
+   }
+   return false;
 };
 
 console.log(containsDuplicate([1, 2, 3, 4])); 
